d7b676e099bb5ea985ea1e306238d18b94344452
addisonc1 <addison.chan21@ycdsbk12.ca>
2020-12-08 05:23:42 +0000

    code hotwired code to be final pair star

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -15,9 +15,17 @@ public static int count7(int n){
 
  }
 public static String pairStar(String str){
- return "hel*lo";
-
-
+ if(str.length() < 2){
+   return str;
+}
+ if (str.charAt(0)== str.charAt(1)){
+   return str.charAt(0)+ "*"+ pairStar(str.substring(1));
+   
+ }else{
+   return str.charAt(0)+ pairStar(str.substring(1));
+ 
+ 
+}
 }
 
   
